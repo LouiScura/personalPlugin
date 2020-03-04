dc01f8df495dbe4a10dc012b09426b96b7c155f2
Luis Scura <luisfscura@gmail.com>
2020-03-04 10:03:05 -0500

    This is a test!

--- Inc/Base/Enqueue.php
+++ Inc/Base/Enqueue.php
@@ -19,4 +19,4 @@ class Enqueue extends BaseController
         wp_enqueue_script('mypluginscript', $this->plugin_url . 'assets/script.min.js');
     }
 }
-//this is just a quickly test!ddd
+//this is just a test for GitLab! 
