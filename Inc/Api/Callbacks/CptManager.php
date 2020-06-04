f449de651772eeda5789401d6a3e16dee57544d3
Luis Scura <luisfscura@gmail.com>
2020-06-04 11:08:44 -0400

    Made changes to be able to stored multiples CTP

--- Inc/Api/Callbacks/CptManager.php
+++ Inc/Api/Callbacks/CptManager.php
@@ -7,9 +7,21 @@ namespace Inc\Api\Callbacks;
 
 class CptManager
 {
-    public function cptSanitize( $input )
+    public function cptSanitize( $input ) //information user fill up 
     {   
-        return $input;
+        $output = get_option('cpt_plugin'); //information on database
+
+        foreach( $output as $key => $value ){ //for each option 
+            if( $input['post_type'] == $key ){
+                // echo "input['post_type'] is iqual to the key ----  ";
+                $output[$key] = $input;
+            }else{
+                // echo "input['post_type'] IS NOT iqual to the key ----  ";
+                $output[ $input[ 'post_type' ] ] = $input;
+            }
+        }
+
+        return $output;
     }
 
     public function cptSectionManager()
@@ -22,9 +34,8 @@ class CptManager
 		$name = $args['label_for'];
 		$option_name = $args['option_name'];
 		$input = get_option( $option_name );
-		$value = $input[ $name ];
 
-        echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="' . $value . '" placeholder="' . $args['placeholder'] . '">';
+        echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="" placeholder="' . $args['placeholder'] . '">';
     }
     
     public function checkboxField( $args )
@@ -32,7 +43,6 @@ class CptManager
         $name = $args['label_for'];
         $classes = $args['class'];
         $option_name = $args['option_name'];
-        // $checked = isset($checkbox[$name] ? true : false);
         $checkbox = get_option(  $option_name );
 
         echo '<div class="' . $classes . '">
